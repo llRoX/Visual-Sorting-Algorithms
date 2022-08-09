f04b0f2fbaa062400b87ccff8e2da62bb60ea1e0
Oliver White <oliver-white@live.com>
2022-08-09 15:07:02 +1000

    Added utility method to check if array is sorted.

--- src/sorting/core/utils.js
+++ src/sorting/core/utils.js
@@ -3,16 +3,25 @@
 
 
 //Function To Swap Two Objects In An Array
-export  function swap(arr, indexA, indexB) {
+export function swap(arr, indexA, indexB) {
     let temp = arr[indexA];
     arr[indexA] = arr[indexB];
     arr[indexB] = temp;
 }
 
 
+export function isSorted(arr) {
+    let last = arr[0]
 
-
-
+    for (let val of arr) {
+        if (val < last) {
+            return false;
+        } else {
+            last = val;
+        }
+    }
+    return true;
+}
 
 
 
